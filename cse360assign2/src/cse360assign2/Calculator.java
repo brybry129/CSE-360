0729a9ea0d07e5984a0a0079a2c0094ec3dcfbae
bdonovan <bdonovan@SDCL0119.sdcclinical.local>
2019-02-25 21:01:52 -0700

    Initial checkin of calculator tool

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,80 @@
+/* Name: Bryanna Donovan
+ * Class ID: 364
+ * Assignment 2
+ */
+
+package cse360assign2;
+
+/**
+ *  The Calculator class will act like a real calculator and add, subtract,
+ * multiply, and divide to return the total after the operations.
+ * 
+ * @author Bryanna Donovan
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/** 
+	 * The constructor of the class Calculator
+	 */
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total after being calculated
+	 * @return	the total
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This will add the parameter to the total value and store it in total.
+	 * @param value	an integer to add to the total
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This will subtract the parameter from the total and store it in total.
+	 * @param value	an integer to subtract from the total
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This will multiply the parameter by the total and store it in total.
+	 * @param value	an integer to multiply to the total
+	 */
+	
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This will divide the total by the parameter and store it in total.
+	 * @param value	an integer to divide the total by
+	 */
+	
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns the history of all actions as a String
+	 * @return	string with all of the history of previous actions
+	 */
+	
+	public String getHistory () {
+		return "";
+	}
+}
